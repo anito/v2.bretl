d2c71e7f573601094f22f479e152a2d15d2901f1
anito <axelnitzschner@mac.com>
2018-09-21 09:32:06 +0200

    scrollTop

--- wp-content/wptouch-data/themes/mobilestore-child/default/js/main.js
+++ wp-content/wptouch-data/themes/mobilestore-child/default/js/main.js
@@ -32,10 +32,87 @@ jQuery( function( $ ) {
             });
         }
 	
-	
+	var add_animate_scroll = function() {
+
+        $('a[href^=#]').on('click', function(e){
+
+            var href = $(this).attr('href');
+
+            if(typeof $(href).offset() == 'undefined') return;
+
+            $('html, body').animate({
+                scrollTop:$(href).offset().top
+            },'slow');
+
+            e.preventDefault();
+
+        });
+
+    };
+
+    var add_check_scroll = function() {
+
+        var timer_id;
+
+        $('body').attr('id', 'the-top');
+
+        $(window).on('scroll', function(e) {
+
+            e.preventDefault();
+
+            var cl = '.scroll-top',
+                height = $(window).height(),
+                scrollTop = $(window).scrollTop();
+
+            if( scrollTop > height ) {
+                div_handler( cl );
+
+                $( cl ).removeClass( 'dimmed' ).addClass( 'active' );;
+
+                clearTimeout( timer_id );
+                timer_id = setTimeout( function() {
+                    $( cl ).addClass( 'dimmed' );
+                }, 4000);
+
+            } else {
+                $( cl ).addClass( 'away' ).removeClass( 'active' );
+
+                if( timer_id ) {
+                    clearTimeout(timer_id);
+                    timer_id = null;
+                }
+            }
+
+
+        });
+
+        var div_handler = function( cl ) {
+
+            var cl, tmpl, el;
+
+            el  = $( cl );
+
+            if( !el.length ) {
+
+                str = cl.replace(/(^\.|\.)/g, " ");
+                tmpl = `<div class="${str} away"><a href="#the-top" class="scroll-top-inner wptouch-icon-angle-up"></a></div>`;
+
+                $('body').append( $(tmpl) );
+                add_animate_scroll();
+
+            }
+
+            el.removeClass( 'away' );
+
+        };
+
+    }
+
 	add_adult_badge();
 	add_background_image();
 	add_search_badge_el();
 	add_readmore();
+    add_animate_scroll();
+    add_check_scroll();
 	
 });
