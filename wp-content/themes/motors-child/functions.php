db54b136b17b659924d4a9db21c2f3c6d0401bad
anito <axelnitzschner@mac.com>
2019-05-22 11:59:39 +0200

    New "motor" theme and translation plugin "loco translate"

---
+++ wp-content/themes/motors-child/functions.php
@@ -0,0 +1,10 @@
+<?php
+require_once( __DIR__ . '/includes/sender_email.php');
+
+add_action( 'wp_enqueue_scripts', 'stm_enqueue_parent_styles' );
+
+function stm_enqueue_parent_styles() {
+
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('stm-theme-style') );
+
+}
