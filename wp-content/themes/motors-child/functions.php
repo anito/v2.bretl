25e345742fd4a3a4a96991f4fbe5927d25085aa9
anito <axelnitzschner@mac.com>
2019-05-23 13:20:54 +0200

    test v2

--- wp-content/themes/motors-child/functions.php
+++ wp-content/themes/motors-child/functions.php
@@ -1,4 +1,6 @@
 <?php
+require_once( __DIR__ . '/includes/product_category_handler.php');
+require_once( __DIR__ . '/includes/duplicate_content.php');
 require_once( __DIR__ . '/includes/sender_email.php');
 
 
