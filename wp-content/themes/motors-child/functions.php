95677b8c415b2bc20e256c365ec3711e001e9763
anito <axelnitzschner@mac.com>
2019-09-26 00:09:06 +0200

    fancybox v3

--- wp-content/themes/motors-child/functions.php
+++ wp-content/themes/motors-child/functions.php
@@ -23,9 +23,10 @@ function stm_enqueue_parent_styles() {
 	/*
 	* Other Libraries
 	*/
-    wp_enqueue_script('fancybox', get_stylesheet_directory_uri() . '/assets/js/fancybox/jquery.fancybox.js', false, false, true);
+    wp_enqueue_script('fancybox', get_stylesheet_directory_uri() . '/vendors/fancybox/dist/jquery.fancybox.js', false, false, true);
+	wp_enqueue_style('fancybox', get_stylesheet_directory_uri() . '/vendors/fancybox/dist/jquery.fancybox.css');
+	
     wp_enqueue_script('fancybox-helper', get_stylesheet_directory_uri() . '/assets/js/fancybox-helper.js', false, false, true);
-    wp_enqueue_style('fancybox', get_stylesheet_directory_uri() . '/assets/css/fancybox/jquery.fancybox.css');
     wp_enqueue_style('fancy-metaslider', get_stylesheet_directory_uri() . '/assets/css/fancy-metaslider.css');
     wp_enqueue_style('dashicons');
 
